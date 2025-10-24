8ef89d28cdd2d041a2a330b31f1553c5e90470f3
Ahsan Abbas <K253085@nu.edu.pk>
2025-10-23 22:27:41 -0700

    Add files via upload

---
+++ Task2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int arr[3][4]=
+	{ 
+	{10,22,35,4}, 
+	{50,65,73,80},
+	{91,10,11,12} 
+	} ;
+
+	int i,j ,max=0 ,row,col;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			if(max<arr[i][j])
+			{
+				max=arr[i][j];
+	             row=i;
+	             col=j;
+			}
+			
+		}
+	}
+	printf("Maximum is %d at Index[%d,%d ]",max,row,col);
+
+	return 0;
+}
+
